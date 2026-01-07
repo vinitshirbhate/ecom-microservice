aad9f29e300b03bc9471934353df54ea0ce4ace3
vinitshirbhate <vinit.shirbhate23@vit.edu>
2026-01-07 23:41:19 +0530

    feat: creating resolvers

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,1 +1,40 @@
 package main
+
+
+type Server struct{
+
+}
+
+func NewGraphqlServer(accountUrl,catalogUrl,orderUrl) (*Server,error) {
+	accountClient ,err += accountClient.NewClient(accountUrl)
+
+	if err != nil {
+		return nil,err
+	}
+
+	catalogClient ,err += catalogClient.NewClient(catalogUrl)
+
+	if err != nil {
+		accountClient.close()
+		return nil,err
+	}
+
+	orderClient ,err += orderClient.NewClient(orderUrl)
+
+	if err != nil {
+		catalogClient.close()
+		return nil,err
+	}
+
+	return &Server{
+		accountClient,
+		catalogClient,
+		orderClient,
+	},nil
+}
+
+func (s *Server) Mutation() MutationResolver{
+	return &mutationResolver{
+		server:s,
+	}
+}
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,1 +1,20 @@
 package main
+
+type mutationResolver struct{
+	server *Server
+}
+
+
+// func (r *mutationResolver) createAccount(ctx context.Context,in AccountInput) (*Account,error) {
+
+// }
+
+
+// func (r *mutationResolver) createProduct(ctx context.Context,in ProductInput) (*Product,error) {
+	
+// }
+
+
+// func (r *mutationResolver) createOrder(ctx context.Context,in OrderInput) (*Order,error) {
+	
+// }
